d55d9791b6271732ac8f97bbddf7cb8646f5aea5
Girish21 <girishkumarv21@gmail.com>
2022-06-25 10:48:12 +0530

    ✨ initialize RR DataRouter

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { DataBrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
@@ -11,10 +12,12 @@ let trpcClient = trpc.createClient({ url: 'http://localhost:8080/trpc' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <App />
-      </trpc.Provider>
-    </QueryClientProvider>
+    <DataBrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <App />
+        </trpc.Provider>
+      </QueryClientProvider>
+    </DataBrowserRouter>
   </React.StrictMode>,
 )
