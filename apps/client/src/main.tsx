22de1c6709e33079a3ced93cb223f0be27fba5e2
Girish21 <girishkumarv21@gmail.com>
2022-06-26 16:30:14 +0530

    🙁 remove tRPC

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { DataBrowserRouter } from 'react-router-dom'
+import { DataBrowserRouter, Route } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
-import { trpc, trpcClient } from './utils/trpc'
 
 let queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DataBrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-          <App />
-        </trpc.Provider>
-      </QueryClientProvider>
-    </DataBrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <DataBrowserRouter>
+        <Route index element={<App />} />
+      </DataBrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
