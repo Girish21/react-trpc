f0f53a1b98308b252fbb377d2350150832a49766
Girish21 <girishkumarv21@gmail.com>
2022-06-28 00:48:04 +0530

    ✨ required trpc routes

--- apps/server/src/trpc.ts
+++ apps/server/src/trpc.ts
@@ -1,11 +1,33 @@
 import * as trpc from '@trpc/server'
 import { z } from 'zod'
+import { db } from './db'
 
-const appRouter = trpc.router().query('hello', {
-  output: z.string(),
-  async resolve() {
-    return 'Hello from tRPC'
-  },
-})
+const noteRoutes = trpc
+  .router()
+  .query('categories', {
+    async resolve() {
+      return await db.noteTag.findMany({
+        select: { _count: true, id: true, tag: true },
+      })
+    },
+  })
+  .query('note', {
+    input: z.string().min(1),
+    async resolve({ input }) {
+      return await db.note.findMany({
+        where: { noteTagId: input },
+        select: {
+          id: true,
+          updatedAt: true,
+          content: true,
+          createdAt: true,
+          title: true,
+          Tag: { select: { tag: true } },
+        },
+      })
+    },
+  })
+
+const appRouter = trpc.router().merge('note.', noteRoutes)
 
 export default appRouter
