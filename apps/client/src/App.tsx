e1ff9e0d6245144ee7539bf9aeefb4db1a2b4d26
Girish21 <girishkumarv21@gmail.com>
2022-06-30 23:48:06 +0530

    SPA form

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,27 @@
 export default function Aoo() {
   return (
     <main className='grid h-full place-content-center'>
-      <h1 className='font-mono text-4xl font-bold'>React + tRPC</h1>
+      <form
+        onSubmit={async e => {
+          // await fetch(`${import.meta.env.VITE_BASE_URL}`, {
+          //   method: 'post',
+          //   body: JSON.stringify({ field }),
+          //   headers: {
+          //     'Content-Type': 'application/json',
+          //   },
+          // })
+        }}
+        method='post'
+      >
+        <input
+          name='field'
+          required
+          className='rounded border border-gray-800 px-2 py-px'
+        />
+        <button className='rounded border border-gray-400 bg-gray-200 px-2 py-px'>
+          Submit
+        </button>
+      </form>
     </main>
   )
 }
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,23 +4,6 @@ import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
 
-const PageHTML = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Form Basic</title>
-    </head>
-    <body>
-      <form enctype="application/x-www-form-urlencoded" method="POST">
-        <input name="field" required="true" />
-        <button>Submit</button>
-      </form>
-    </body>
-  </html>
-`
-
 dotenv.config()
 
 let app = express()
@@ -33,15 +16,11 @@ app.use(cors())
 
 app.use(morgan('tiny'))
 
-app.use(express.urlencoded())
-
-app.get('/', (_, res: express.Response) => {
-  return res.status(200).send(PageHTML)
-})
+app.use(express.json())
 
 app.post('/', async (req: express.Request, res: express.Response) => {
   console.log(req.body)
-  return res.status(200).end(PageHTML)
+  return res.status(200).send()
 })
 
 app.listen(process.env.PORT, () => {
