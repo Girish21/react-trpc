c73c930dcd496edafcbb293de65dde154db8897b
Girish21 <girishkumarv21@gmail.com>
2022-06-30 22:21:32 +0530

    🚀 `form` demo

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,23 @@ import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
 
+const PageHTML = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Form Basic</title>
+    </head>
+    <body>
+      <form enctype="application/x-www-form-urlencoded" method="POST">
+        <input name="field" required="true" />
+        <button>Submit</button>
+      </form>
+    </body>
+  </html>
+`
+
 dotenv.config()
 
 let app = express()
@@ -16,8 +33,15 @@ app.use(cors())
 
 app.use(morgan('tiny'))
 
+app.use(express.urlencoded())
+
 app.get('/', (_, res: express.Response) => {
-  return res.status(200).send('Hello World!')
+  return res.status(200).send(PageHTML)
+})
+
+app.post('/', async (req: express.Request, res: express.Response) => {
+  console.log(req.body)
+  return res.status(200).end(PageHTML)
 })
 
 app.listen(process.env.PORT, () => {
